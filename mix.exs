2a8aac6e34b81da0f961adeab6178d4701b61b54
Chris Maddox <tyre77@gmail.com>
2015-07-23 11:46:35 -0700

    prepare 0.1.0 release

--- mix.exs
+++ mix.exs
@@ -5,27 +5,27 @@ defmodule Relisa.Mixfile do
     [app: :relisa,
      version: "0.1.0",
      elixir: "~> 1.0",
+     description: "Fast, simple, and composable deployment library for Elixir.",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps]
+     source_url: "https://github.com/SenecaSystems/relisa",
+     deps: deps,
+     package: package]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type `mix help compile.app` for more information
   def application do
     [applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type `mix help deps` for more examples and options
+  defp package do
+    [
+      files: ["lib", "mix.exs", "README.md", "LICENSE"],
+      contributors: ["Seneca Systems"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/SenecaSystems/relisa"}
+    ]
+  end
+
   defp deps do
     [{:exrm, "~> 0.18.5"}]
   end
